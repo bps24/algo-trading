72c3ef845f78bafbaa851c13e76fde6869de2906
jbaldi13 <jbaldi13@optimum.net>
2021-03-30 00:23:32 -0400

    Create close_positions.py

---
+++ functions/close_positions.py
@@ -0,0 +1,27 @@
+import shift
+
+def close_positions(trader: shift.Trader, ticker: str = "ALL"):
+    if ticker == "ALL":
+        trader.cancel_all_pending_orders()
+        for item in trader.get_portfolio_items():
+            close_positions(trader, item)
+
+    elif ticker == "SHORT":
+        for item in trader.get_portfolio_items().values():
+            if item.get_shares() < 0:
+                close_positions(trader, item.get_symbol())
+
+    elif ticker == "LONG":
+        for item in trader.get_portfolio_items().values():
+            if item.get_shares() > 0:
+                close_positions(trader, item.get_symbol())
+
+    else:
+        item = trader.get_portfolio_item(ticker)
+        if item.get_shares() > 0:
+            close_long = shift.Order(shift.Order.Type.MARKET_SELL, item.get_symbol(), int(item.get_shares() / 100))
+            trader.submit_order(close_long)
+        elif item.get_shares() < 0:
+            cover_short = shift.Order(shift.Order.Type.MARKET_BUY, item.get_symbol(), int(item.get_shares() / -100))
+            trader.submit_order(cover_short)
+        print("All", ticker, "positions exited")
