aec785c7671875f0aaf8f8946ae0d780743326cb
jbaldi13 <jbaldi13@optimum.net>
2021-03-30 00:23:38 -0400

    Update run.py

--- run.py
+++ run.py
@@ -5,15 +5,15 @@ import numpy as np
 import datetime as dt
 import shift
 
+sys.path.insert(1, './functions')
+from portfolio_summary import portfolio_summary
+from close_positions import close_positions
+
 
 def main(argv):
-    trader = shift.Trader("democlient")
+    trader = shift.Trader(credentials.user)
     try:
-<<<<<<< Updated upstream
-        trader.connect(credentials.user, credentials.password)
-=======
-        trader.connect("initiator.cfg", "password")
->>>>>>> Stashed changes
+        trader.connect("initiator.cfg", credentials.password)
     except shift.IncorrectPasswordError as e:
         print(e)
     except shift.ConnectionTimeoutError as e:
@@ -23,7 +23,16 @@ def main(argv):
     aapl_mb = shift.Order(shift.Order.Type.MARKET_BUY, "AAPL", 1)
     trader.submit_order(aapl_mb)
     print('trade completed')
+    time.sleep(3)
+    portfolio_summary(trader)
+    xom_mb = shift.Order(shift.Order.Type.MARKET_BUY, "XOM", 1)
+    trader.submit_order(xom_mb)
+    time.sleep(3)
+    portfolio_summary(trader)
 
+    close_positions(trader, "ALL")
+    time.sleep(3)
+    portfolio_summary(trader)
     time.sleep(200)
 
     trader.disconnect()
