b385a340124d690d4c7da2bbb3445376a963e393
jbaldi13 <jbaldi13@optimum.net>
2021-03-30 21:19:38 -0400

    Update transactionSummary.py

--- functions/transactionSummary.py
+++ functions/transactionSummary.py
@@ -1,4 +1,5 @@
 import shift
 
-def transaction_summary(trader: shift.Trader):
+def transaction_summary(trader: shift.Trader, order: shift.Order):
+    print(f"Order: {order.id}, Symbol: {order.symbol}, Type: {order.type}, Price: {order.price}, Size: {order.size}")
     return
