adbdf8050f0383aa5c5aaa86d7540dacca695b93
jbaldi13 <jbaldi13@optimum.net>
2021-03-30 22:13:58 -0400

    Delete transactionSummary.py

--- functions/transactionSummary.py
+++
@@ -1,5 +0,0 @@
-import shift
-
-def transaction_summary(trader: shift.Trader, order: shift.Order):
-    print(f"Order: {order.id}, Symbol: {order.symbol}, Type: {order.type}, Price: {order.price}, Size: {order.size}")
-    return
